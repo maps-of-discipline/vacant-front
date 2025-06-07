37cb1a32805c2e3d60eaecc491204e06dd3e069d
De333mond <vladb539@gmail.com>
2025-06-07 12:59:30 +0300

    update rups panel

--- src/components/rups/RupControlPanel.vue
+++ src/components/rups/RupControlPanel.vue
@@ -13,66 +13,41 @@
           <div class="flex justify-content-between">
             <div class="flex gap-2">
               <span>Совпадающие</span>
-              <Checkbox
-                :model-value="model.showMatching"
-                binary
-                @update:model-value="updateModel('showMatching', $event)"
-              />
+              <Checkbox :model-value="model.showMatching" binary
+                @update:model-value="updateModel('showMatching', $event)" />
             </div>
             <div class="flex gap-2">
               <span>С вариантами</span>
-              <Checkbox
-                :model-value="model.showWithVariants"
-                binary
-                @update:model-value="updateModel('showWithVariants', $event)"
-              />
+              <Checkbox :model-value="model.showWithVariants" binary
+                @update:model-value="updateModel('showWithVariants', $event)" />
             </div>
             <div class="flex gap-2">
               <span>Без вариантов</span>
-              <Checkbox
-                :model-value="model.showWithoutVariants"
-                binary
-                @update:model-value="updateModel('showWithoutVariants', $event)"
-              />
+              <Checkbox :model-value="model.showWithoutVariants" binary
+                @update:model-value="updateModel('showWithoutVariants', $event)" />
             </div>
           </div>
         </div>
         <div class="flex gap-2 align-items-center">
           <span class="font-semibold">Режим отображения:</span>
-          <SelectButton
-            class="m-auto"
-            :model-value="model.mode"
-            :options="modeOptions"
-            option-label="name"
-            option-value="value"
-            @update:model-value="updateModel('mode', $event)"
-          />
+          <SelectButton class="m-auto" :model-value="model.mode" :options="modeOptions" option-label="name"
+            option-value="value" @update:model-value="updateModel('mode', $event)" />
         </div>
 
         <div class="flex gap-2 align-items-center w-full">
           <span class="font-semibold w-full">Мин. схожесть: ({{ model.threshold }})</span>
-          <Slider
-            class="w-full"
-            :model-value="model.threshold"
-            :min="0.01"
-            :max="1.0"
-            :step="0.01"
-            @update:model-value="updateModel('threshold', $event)"
-          />
+          <Slider class="w-full" :model-value="model.threshold" :min="0.01" :max="1.0" :step="0.01"
+            @update:model-value="updateModel('threshold', $event)" />
         </div>
         <div class="flex gap-2 align-items-center">
           <span class="font-semibold">С понижением курса</span>
-          <Checkbox
-            :model-value="model.withLowCourse"
-            binary
-            @update:model-value="updateModel('withLowCourse', $event)"
-          />
+          <Checkbox :model-value="model.withLowCourse" binary
+            @update:model-value="updateModel('withLowCourse', $event)" />
         </div>
-        <ElectiveModal
-          v-model="model.choosenElectives"
-          :rup-data="props.rupData"
-        />
       </div>
+    </Panel>
+    <Panel header="Элективы">
+      <ElectiveModal v-model="model.choosenElectives" :rup-data="props.rupData" />
     </Panel>
   </div>
 </template>
