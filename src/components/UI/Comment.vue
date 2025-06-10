50dffb4f4f5f230d19ca70970727a20f639264ea
De333mond <vladb539@gmail.com>
2025-06-10 19:41:41 +0300

    fix comment header

--- src/components/UI/Comment.vue
+++ src/components/UI/Comment.vue
@@ -1,35 +1,21 @@
 <template>
-  <div
-    :class="{
-      'pl-4': authStore.payload.user_id == comment.by_id,
-      'pr-4': authStore.payload.user_id != comment.by_id,
-    }"
-  >
-    <Panel
-      :header="props.comment.by"
-      class="mb-3 w-fit"
-      :class="{
-        'ml-auto': authStore.payload.user_id == comment.by_id,
-      }"
-    >
+  <div :class="{
+    'pl-4': authStore.payload.user_id == comment.by_id,
+    'pr-4': authStore.payload.user_id != comment.by_id,
+  }">
+    <Panel :header="props.comment.by" class="mb-3 w-fit" :class="{
+      'ml-auto': authStore.payload.user_id == comment.by_id,
+    }">
       <template #header>
-        <span
-          v-if="props.type === 'stuff'"
-          class="pi pi-eye-slash mr-2"
-        ></span>
-        <span class="font-semibold">{{ props.comment.by }}</span>
+        <div class="flex mr-auto">
+          <span v-if="props.type === 'stuff'" class="pi pi-eye-slash mr-2"></span>
+          <span class="font-semibold">{{ props.comment.by }}</span>
+        </div>
       </template>
       <template #icons>
         <div class="pl-3">
-          <Button
-            v-if="authStore.payload.user_id == comment.by_id && canDeleteComments"
-            icon="pi pi-trash"
-            rounded
-            size="small"
-            text
-            severity="secondary"
-            @click="emit('remove')"
-          />
+          <Button v-if="authStore.payload.user_id == comment.by_id && canDeleteComments" icon="pi pi-trash" rounded
+            size="small" text severity="secondary" @click="emit('remove')" />
         </div>
       </template>
       {{ props.comment.text }}
